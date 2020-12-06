955752dee3cc9ff5edee840fc408d3f2d526a548
zhangshuai <zhangshuai08@sunlands.com>
2020-12-06 18:44:57 +0800

    feat: 修改完成

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,10 @@ import { join } from 'path';
 
 export default defineConfig({
   title: 'z-react-ui',
-  favicon: 'http://m.qpic.cn/psc?/V13sL17e2IskgF/ruAMsa53pVQWN7FLK88i5panvpKM.uKn2D6a3PfJMjCKBgHJtiIq.zj.7d2hdbLGfaAxjUP2yNouZ2AR4uC6bRjy368JEQ4sFaLwqIB2g4k!/b&bo=9AHtAQAAAAABBzk!&rf=viewer_4',
-  logo: 'http://m.qpic.cn/psc?/V13sL17e2IskgF/ruAMsa53pVQWN7FLK88i5panvpKM.uKn2D6a3PfJMjCKBgHJtiIq.zj.7d2hdbLGfaAxjUP2yNouZ2AR4uC6bRjy368JEQ4sFaLwqIB2g4k!/b&bo=9AHtAQAAAAABBzk!&rf=viewer_4',
+  favicon:
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607261513322&di=cf3cdcc164c5f324e73fe431a0fbf139&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Fart_origin_min_pic%2F19%2F03%2F16%2Fa2aa2dcfedd10471da72a2dd9aca0d4c.jpg',
+  logo:
+    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607261513322&di=cf3cdcc164c5f324e73fe431a0fbf139&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Fart_origin_min_pic%2F19%2F03%2F16%2Fa2aa2dcfedd10471da72a2dd9aca0d4c.jpg',
   base: '/dumi-father-react-ui',
   publicPath: '/dumi-father-react-ui/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
